401c5628634f7e6730763e255d3d9082233c86a4
Al <royalguard1994@mail.ru>
2019-03-16 21:59:37 +0200

    Added method with do-while-cycle. Added massive Fibonacci numbers

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,8 +1,10 @@
 public class Fibonacci {
 
+    private static final int FIBONACCI_NUMBERS = 11;
     private static int previous = 0;
     private static int current;
     private static int nextOne;
+    private static int massive[] = new int[FIBONACCI_NUMBERS];
 
     public static void fibonacciFor() {
 
@@ -11,5 +13,20 @@ public class Fibonacci {
             System.out.print(current + " ");
             previous = current;
         }
+        System.out.print("\n");
+    }
+    public static void fibonacciMassiveDoWhile() {
+        previous = 0;
+        current = 1;
+        int count = 0;
+
+        do {
+            nextOne = previous + current;
+            massive[count] = current;
+            previous = current;
+            current = nextOne;
+            System.out.print(massive[count] + " ");
+            count++;
+        } while (current < 100);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String args[]) {
+
         Fibonacci fibonacci = new Fibonacci();
 
         fibonacci.fibonacciFor();
+
+        fibonacci.fibonacciMassiveDoWhile();
     }
 }
